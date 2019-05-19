6bc9bfcec3a747ab2478d0f95aab0323011fe1f3
Tatsuki-I <tatsuki.devel@gmail.com>
2019-05-19 20:51:00 +0900

    first commit

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -10,3 +10,4 @@ ln -sf ~/dotfiles/.brewfile/Brewfile ~/Brewfile
 ln -sf ~/dotfiles/.vim/colors/hybrid.vim ~/.vim/colors/hybrid.vim
 ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
 ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+ln -sf ~/dotfiles/.stack/config.yaml ~/.stack/config.yaml
